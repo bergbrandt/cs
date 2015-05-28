f39b6e83a3cb1faa22f13fd2547ba4ffff6bdd43
Berg Brandt <berg@yahoo-inc.com>
2015-05-28 11:01:38 -0700

    Improved Binary Search (droping array.slice and using pointer management)

--- search/binary-search/binary-search.js
+++ search/binary-search/binary-search.js
@@ -4,24 +4,21 @@
  * @param {Array} arr sorted array to be searched
  * @return {(number|boolean)} the index where the element was found or false
  */
-var binarySearch = function(x, arr) {
+var binarySearch = function(x, arr, iS, iE) {
     var n = arr.length;
-    var m = Math.floor(n/2);
-    var r = false; // default is false
+    var iS = iS || 0;
+    var iE = iE || n-1;
+    var m = iS + Math.floor((iE - iS)/2);
 
-    // if the array is empty or search number is out of range
-    if (n === 0 || x < arr[0] || x > arr[n-1]) {
+    if (n === 0 || // if the array is empty
+        x < arr[0] || x > arr[n-1] || // OR search number is out of range
+        (iE === iS && x !== arr[m])) { // OR range is already unitary but a match hasn't been found
         return false;
-    }
-
-    if (x < arr[m]) { // If search element is less than the middle element, continue on the left half
-        r = binarySearch(x, arr.slice(0, m));
+    } else if (x === arr[m]) { // If a match is found
+        return  m;
+    } else if (x < arr[m]) { // If search element is less than the middle element, continue on the left half
+        return  binarySearch(x, arr, iS, m);
     } else if(x > arr[m]) { // If search element is greater than the middle element, continue on the right half
-        r = binarySearch(x, arr.slice(m, n));
-        r = r === false ? false : r + m; // If result if not false, at m to obtain the final index
-    } else if (x === arr[m]) {
-        r = m;
-    } 
-
-    return r;
+        return binarySearch(x, arr, m + 1, iE);
+    }
 }
